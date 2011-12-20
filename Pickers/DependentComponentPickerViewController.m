f8f445f41e450eeb09fb5a2dab844e6bd9e387d6
Saurabh Jain <saurabhjain.net@gmail.com>
2011-12-20 10:59:46 -0800

    Initial Push for Dependent picker

--- Pickers/DependentComponentPickerViewController.m
+++ Pickers/DependentComponentPickerViewController.m
@@ -10,6 +10,64 @@
 
 
 @implementation DependentComponentPickerViewController
+@synthesize picker;
+@synthesize stateZips;
+@synthesize states;
+@synthesize zips;
+
+- (IBAction)buttonPressed {
+    
+    NSInteger stateRow = [picker selectedRowInComponent:kStateComponent];
+    NSInteger zipRow = [picker  selectedRowInComponent:kZipComponent];
+    
+    NSString *state = [self.states objectAtIndex:stateRow];
+    NSString *zip = [self.zips objectAtIndex:zipRow];
+    
+    NSString *title = [[NSString alloc] initWithFormat:@"You selected a zip code: %@", zip];
+    NSString *message = [[NSString alloc] initWithFormat:@"%@ is in %@", zip, state];
+    
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title message:message delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+    
+    [alert show];
+    [alert release];
+    [title release];
+    [message release];
+    
+}
+
+# pragma mark -
+# pragma Picker Data Source Methods
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    
+    return 2;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    
+    if (component == kStateComponent)
+        return [self.states count];
+    return [self.zips count];
+}
+
+# pragma mark Picker Delegate Methods
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    
+    if (component == kStateComponent) 
+        return [self.states objectAtIndex:row];
+    return [self.zips objectAtIndex:row];
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+    if (component == kStateComponent) {
+        NSString *selectedState = [self.states objectAtIndex:row];
+        NSArray *array = [stateZips objectForKey:selectedState];
+        self.zips = array;
+        [picker selectRow:0 inComponent:kZipComponent animated:YES];
+        [picker reloadComponent:kZipComponent];
+    }
+    
+}
+
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -22,6 +80,11 @@
 
 - (void)dealloc
 {
+    
+    [picker release];
+    [stateZips release];
+    [states release];
+    [zips release];
     [super dealloc];
 }
 
@@ -37,6 +100,21 @@
 
 - (void)viewDidLoad
 {
+    NSBundle *bundle = [NSBundle mainBundle];
+    NSString *plistPath = [bundle pathForResource:@"statedictionary" ofType:@"plist"];
+    
+    NSDictionary *dictionary = [[NSDictionary alloc] initWithContentsOfFile:plistPath];
+    self.stateZips = dictionary;
+    [dictionary release];
+    
+    NSArray *components = [self.stateZips allKeys];
+    NSArray *sorted = [components sortedArrayUsingSelector:@selector(compare:)];
+    self.states = sorted;
+    
+    NSString *selectedState = [self.states objectAtIndex:0];
+    NSArray *array = [stateZips objectForKey:selectedState];
+    self.zips = array;
+    
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
 }
