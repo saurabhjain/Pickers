c6218a4a015d9692d77c529ff2145bcbcfaea576
Saurabh Jain <saurabhjain.net@gmail.com>
2011-12-19 16:11:00 -0800

    Changes for xib done in the dependent pikcer

--- Pickers/DependentComponentPickerViewController.h
+++ Pickers/DependentComponentPickerViewController.h
@@ -8,9 +8,24 @@
 
 #import <UIKit/UIKit.h>
 
+#define kStateComponent 0
+#define kZipComponent 1
 
-@interface DependentComponentPickerViewController : UIViewController {
+
+@interface DependentComponentPickerViewController : UIViewController <UIPickerViewDelegate, UIPickerViewDataSource> {
+    
+    IBOutlet UIPickerView *picker;
+    NSDictionary *stateZips;
+    NSArray *states;
+    NSArray *zips;
     
 }
+
+@property(retain, nonatomic) UIPickerView *picker;
+@property(retain, nonatomic) NSDictionary *stateZips;
+@property(retain, nonatomic) NSArray *states;
+@property(retain, nonatomic) NSArray *zips;
+
+- (IBAction)buttonPressed;
 
 @end
