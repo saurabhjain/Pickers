7862e83ff851ec794e477b494de68b0a10207f8e
Saurabh Jain <saurabhjain.net@gmail.com>
2011-12-20 13:09:17 -0800

    Header Created for Slot Picker

--- Pickers/CustomComponentPickerViewController.h
+++ Pickers/CustomComponentPickerViewController.h
@@ -9,8 +9,26 @@
 #import <UIKit/UIKit.h>
 
 
-@interface CustomComponentPickerViewController : UIViewController {
+@interface CustomComponentPickerViewController : UIViewController <UIPickerViewDataSource, UIPickerViewDelegate> {
     
+    IBOutlet UIPickerView *picker;
+    IBOutlet UILabel *winLabel;
+    
+    NSArray *column1;
+    NSArray *column2;
+    NSArray *column3;
+    NSArray *column4;
+    NSArray *column5;
 }
+
+@property(nonatomic, retain) UIPickerView *picker;
+@property(nonatomic, retain) UILabel *winLabel;
+@property(nonatomic, retain) NSArray *column1;
+@property(nonatomic, retain) NSArray *column2;
+@property(nonatomic, retain) NSArray *column3;
+@property(nonatomic, retain) NSArray *column4;
+@property(nonatomic, retain) NSArray *column5;
+
+- (IBAction)spin;
 
 @end
